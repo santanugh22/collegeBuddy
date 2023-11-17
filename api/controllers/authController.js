c7fed0d5873b24698e06b64b2b00eb7f73ee563d
santanugh22 <11463046+santanugh22@users.noreply.github.com>
2023-11-18 00:21:50 +0530

    LOGIN/REGISTER SYSTEM NOW DONE

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,91 +1,99 @@
 import pool from "../db/Connection.js";
+import bcrypt from "bcrypt";
+import { jwtGenerator, jwtVerifier } from "../utils/jwtGenerator.js";
+
 export const loginController = async (req, res) => {
   try {
-    const {username,password}=req.body
+    const { email, password } = req.body;
 
+    // lets check if the user exist or not in the table
+    const user = await pool.query(
+      `SELECT * FROM users_basic WHERE email=$1 AND password=$2`,
+      [email, password]
+    );
+    if (user.rows[0].length==0) {
+      res.status(403).json("User doesn't exist");
+    } else {
+      const user_new = user.rows[0];
 
-    // lets check if the user exist or not in the table 
-    const user=await pool.query('SELECT username FROM users2 WHERE username=$1 AND password=$2',[username,password])
-    if(!user){
-      res.status(403).json("User doesn't Exist")
+      const jwtToken = await jwtGenerator(
+        user_new.user_id,
+        user_new.email,
+        user_new.first_name,
+        user_new.last_name
+      );
+
+      res.status(200).json(jwtToken);
     }
-
-    res.status(200).json("Logged in successfully")
-
-    
   } catch (error) {
-    res.status(400).json(error)
-    
+    res.status(error.status).json(error);
   }
 };
 
 export const registerController = async (req, res) => {
   try {
-    const {username,email,password}=req.body
+    const { first_name, last_name, email, password } = req.body;
 
-    // check if the user already exist 
-    const userCheck=await pool.query("SELECT username FROM users2 WHERE username=$1 and email=$2",[username,email])
-    if(userCheck.rows[0]){
+    // check if the user already exist
+    const userCheck = await pool.query(
+      "SELECT * FROM users_basic WHERE first_name=$1 and email=$2",
+      [first_name, email]
+    );
+    if (userCheck.rows[0]) {
       res.status(200).json("User already exists");
-     
-     
+    } else {
+      const user = await pool.query(
+        "INSERT INTO users_basic (first_name,last_name,email,password) Values ($1,$2,$3,$4)RETURNING *",
+        [first_name, last_name, email, password]
+      );
+      const user_new = user.rows[0];
 
-    }else{
-        const user = await pool.query(
-          "INSERT INTO users2 (username,password,email) Values ($1,$2,$3)RETURNING *",
-          [username, password, email]
-        );
-        res.status(200).json(user.rows[0]);
-      
+      const jwtToken = await jwtGenerator(
+        user_new.user_id,
+        user_new.email,
+        user_new.first_name,
+        user_new.last_name
+      );
 
+      res.status(200).json(jwtToken);
     }
-   
-     
-  
- 
-
-    
   } catch (error) {
     res.status(403).json(error);
   }
 };
 
-
-
-export const updatePassword=async(req,res)=>{
+export const updatePassword = async (req, res) => {
   try {
-    // check if the user actually exist 
-    const {username,password,newpassword}=req.body
-    const user=await pool.query('SELECT username FROM users WHERE username=$1 AND password=$2',[username,password])
-    if(user.rows[0]){
-      const updatePass=await pool.query('UPDATE users SET password=$1 WHERE username=$2 AND password=$3 RETURNING *',[newpassword,username,password])
-      if(updatePass.rows[0]){
-        res.status(200).json("Success")
-      }else{
-        res.status(300).json("Failed")
+    // check if the user actually exist
+    const { username, password, newpassword } = req.body;
+    const user = await pool.query(
+      "SELECT username FROM users WHERE username=$1 AND password=$2",
+      [username, password]
+    );
+    if (user.rows[0]) {
+      const updatePass = await pool.query(
+        "UPDATE users SET password=$1 WHERE username=$2 AND password=$3 RETURNING *",
+        [newpassword, username, password]
+      );
+      if (updatePass.rows[0]) {
+        res.status(200).json("Success");
+      } else {
+        res.status(300).json("Failed");
       }
-
-    }else{
-      res.status(403).json("U are not allowed to do this")
+    } else {
+      res.status(403).json("U are not allowed to do this");
     }
+  } catch (error) {}
+};
 
+export const allUsers =
+  ("/",
+  async (req, res) => {
+    try {
+      const users = await pool.query(`SELECT * FROM users_basic`);
 
-
-    
-  } catch (error) {
-    
-  }
-}
-
-export const allUsers=('/',async(req,res)=>{
-  try {
-    const users=await pool.query(`SELECT * FROM users_basic`)
-    
-    res.status(200).json(users)
-    
-  } catch (error) {
-    console.log(error)
-    
-  }
-
-})
+      res.status(200).json(users.rows);
+    } catch (error) {
+      console.log(error);
+    }
+  });
---
+++ api/utils/jwtGenerator.js
@@ -0,0 +1,22 @@
+import jsonwebtoken from 'jsonwebtoken'
+const jwt=jsonwebtoken
+import dotenv from 'dotenv'
+dotenv.config()
+async function jwtGenerator(user_id,user_email,first_name,last_name){
+    const data={
+        user_email,user_id,first_name,last_name
+    }
+    return jwt.sign(data,process.env.JWT_PASS,{expiresIn:"2 days"})
+
+}
+
+
+
+async function jwtVerifier(token){
+    const response=jwt.verify(token,process.env.JWT_PASS)
+    return response
+    
+
+}
+
+export {jwtGenerator,jwtVerifier}
